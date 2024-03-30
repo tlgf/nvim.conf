f652efcf252061f632918c63063dd63200fb968a
tom <6648717+tlgf@users.noreply.github.com>
2024-03-30 15:45:57 +1100

    More keybinds

--- init.lua
+++ init.lua
@@ -8,7 +8,9 @@ vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+vim.keymap.set("n", "<leader>fs", builtin.current_buffer_fuzzy_find, {})
 
+vim.cmd [[colorscheme peachpuff]]
 
 vim.api.nvim_set_keymap('n', 'gr', '<cmd>lua require("telescope.builtin").lsp_references()<cr>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', 'gq', '<cmd>lua require("telescope.builtin").lsp_document_symbols()<cr>', { noremap = true, silent = true })
